be5ffc0d08822c7a1c93bfbbf7b0d9a3c77c01f8
artfil <artemf29@icloud.com>
2021-07-02 20:52:38 +0300

    test

--- src/main/java/com/artfil/to/UserTo.java
+++ src/main/java/com/artfil/to/UserTo.java
@@ -10,7 +10,6 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.io.Serial;
 import java.io.Serializable;
 
 import static org.hibernate.validator.constraints.SafeHtml.WhiteListType.NONE;
@@ -18,7 +17,6 @@ import static org.hibernate.validator.constraints.SafeHtml.WhiteListType.NONE;
 @Value
 @EqualsAndHashCode(callSuper = true)
 public class UserTo extends BaseTo implements HasIdAndEmail, Serializable {
-    @Serial
     private static final long serialVersionUID = 1L;
 
     @NotBlank
